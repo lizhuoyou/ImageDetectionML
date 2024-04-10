e9f1073d10ea5c625a95889e3a61a1bc6052f3ea
Daniel Mao <danielmao2019@gmail.com>
2024-04-10 10:59:29 -0400

    [Transforms] Refactor ResizeBBoxes to use relative coordinates

--- transforms/resize_bboxes.py
+++ transforms/resize_bboxes.py
@@ -4,27 +4,26 @@ import torch
 
 class ResizeBBoxes:
 
-    def __init__(self, scale_factor: Tuple[float, float], target_size: Tuple[int, int]) -> None:
+    def __init__(self, scale_factor: Tuple[float, float]) -> None:
         r"""
         Args:
             scale_factor (Tuple[float, float]): The scale factor at which the bounding box coordinated will be resized.
-            target_size (Tuple[int, int]): The size of the entire scene after resize. Used for clipping.
+                Assumed in format (height, width).
         """
         assert type(scale_factor) == tuple, f"{type(scale_factor)=}"
         assert len(scale_factor) == 2, f"{len(scale_factor)=}"
+        assert type(scale_factor[0]) == type(scale_factor[1]) == float, f"{type(scale_factor[0])=}, {type(scale_factor[1])=}"
         self.scale_factor = scale_factor
-        assert type(target_size) == tuple, f"{type(target_size)=}"
-        assert len(target_size) == 2, f"{len(target_size)=}"
-        self.target_size = target_size
 
-    def __call__(self, boxes: torch.Tensor) -> torch.Tensor:
+    def __call__(self, bboxes: torch.Tensor) -> torch.Tensor:
         r"""
         Args:
-            boxes (torch.Tensor): Bounding box annotations. Assumed to be in format (x1, y1, x2, y2).
+            boxes (torch.Tensor): Bounding box annotations. Assumed in format (x1, y1, x2, y2).
         """
-        assert len(boxes.shape) == 2 and boxes.shape[1] == 4, f"{boxes.shape=}"
-        boxes[:, 1] = torch.clip(boxes[:, 1]*self.scale_factor[0], min=0, max=self.target_size[0])
-        boxes[:, 3] = torch.clip(boxes[:, 3]*self.scale_factor[0], min=0, max=self.target_size[0])
-        boxes[:, 0] = torch.clip(boxes[:, 0]*self.scale_factor[1], min=0, max=self.target_size[1])
-        boxes[:, 2] = torch.clip(boxes[:, 2]*self.scale_factor[1], min=0, max=self.target_size[1])
-        return boxes
+        assert type(bboxes) == torch.Tensor, f"{type(bboxes)=}"
+        assert bboxes.dim() == 2 and bboxes.shape[1] == 4, f"{bboxes.shape=}"
+        assert torch.is_floating_point(bboxes), f"{bboxes.dtype=}"
+        assert 0 <= bboxes.min() <= bboxes.max() <= 1, f"{bboxes.min()=}, {bboxes.max()=}"
+        bboxes[:, 0::2] = torch.clamp(bboxes[:, 0::2]*self.scale_factor[1], min=0, max=1)
+        bboxes[:, 1::2] = torch.clamp(bboxes[:, 1::2]*self.scale_factor[0], min=0, max=1)
+        return bboxes
