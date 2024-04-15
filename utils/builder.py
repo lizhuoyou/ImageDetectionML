eb8f008fd3d890ea6ed6ccec97ed544db2098e84
Daniel Mao <danielmao2019@gmail.com>
2024-04-15 11:38:20 -0400

    [Utils] Minor updates

--- utils/builder.py
+++ utils/builder.py
@@ -3,5 +3,5 @@ from typing import Dict, Any
 
 def build_from_config(config: Dict[str, Any], **kwargs) -> Any:
     assert type(config) == dict, f"{type(config)=}"
-    assert 'class' in config and 'args' in config, f"{config.keys()}"
+    assert 'class' in config and 'args' in config, f"{config.keys()=}"
     return config['class'](**config['args'], **kwargs)
