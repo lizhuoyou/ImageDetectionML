6584939a232226f31cd69db80df0d7a02171f561
Daniel Mao <danielmao2019@gmail.com>
2024-04-16 10:52:17 -0400

    [Datasets][Diffusers] Implement diffuser in CCDM paper

--- datasets/diffusers/__init__.py
+++ datasets/diffusers/__init__.py
@@ -4,10 +4,12 @@ DATASETS.DIFFUSERS API
 from datasets.diffusers.base_diffuser import BaseDiffuser
 from datasets.diffusers.object_detection_diffuser import ObjectDetectionDiffuser
 from datasets.diffusers.semantic_segmentation_diffuser import SemanticSegmentationDiffuser
+from datasets.diffusers.ccdm_diffuser import CCDMDiffuser
 
 
 __all__ = (
     'BaseDiffuser',
     'ObjectDetectionDiffuser',
     'SemanticSegmentationDiffuser',
+    'CCDMDiffuser',
 )
