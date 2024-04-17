1fd5e2aa3af9123cf6620794bb7a9649148d319e
Daniel Mao <danielmao2019@gmail.com>
2024-04-16 21:20:20 -0400

    [Criteria][CCDM] Move theta_post_prob into model forward method

--- criteria/ccdm_criterion.py
+++ criteria/ccdm_criterion.py
@@ -41,54 +41,22 @@ class CCDMCriterion(BaseCriterion):
         theta_post = theta / theta.sum(dim=1, keepdim=True)
         return theta_post
 
-    def theta_post_prob(self, diffused_mask: torch.Tensor, pred_mask: torch.Tensor, time: torch.Tensor) -> torch.Tensor:
-        """
-        This is equivalent to calling theta_post with all possible values of x0
-        from 0 to C-1 and multiplying each answer times theta_x0[:, c].
-
-        This should be used when x0 is unknown and what you have is a probability
-        distribution over x0. If x0 is one-hot encoded (i.e., only 0's and 1's),
-        use theta_post instead.
-        """
-        assert diffused_mask.shape == pred_mask.shape, f"{diffused_mask.shape=}, {pred_mask.shape=}"
-        assert 0 <= time.min() <= time.max() < self.num_steps, f"{time=}, {self.num_steps=}"
-        alphas_t = self.alphas[time][..., None, None, None]
-        cumalphas_tm1 = self.alphas_cumprod[time - 1][..., None, None, None, None]
-        alphas_t[time == 0] = 0.0
-        cumalphas_tm1[time == 0] = 1.0
-
-        # We need to evaluate theta_post for all values of x0
-        x0 = torch.eye(self.num_classes, device=diffused_mask.device)[None, :, :, None, None]
-        # theta_xt_xtm1.shape == [B, C, H, W]
-        theta_xt_xtm1 = alphas_t * diffused_mask + (1 - alphas_t) / self.num_classes
-        # theta_xtm1_x0.shape == [B, C1, C2, H, W]
-        theta_xtm1_x0 = cumalphas_tm1 * x0 + (1 - cumalphas_tm1) / self.num_classes
-
-        aux = theta_xt_xtm1[:, :, None] * theta_xtm1_x0
-        # theta_xtm1_xtx0 == [B, C1, C2, H, W]
-        theta_xtm1_xtx0 = aux / aux.sum(dim=1, keepdim=True)
-
-        # theta_x0.shape = [B, C, H, W]
-
-        return torch.einsum("bcdhw,bdhw->bchw", theta_xtm1_xtx0, pred_mask)
-
     def __call__(self, y_pred: torch.Tensor, y_true: Dict[str, torch.Tensor]) -> torch.Tensor:
         assert type(y_pred) == torch.Tensor, f"{type(y_pred)=}"
         assert type(y_true) == dict, f"{type(y_true)=}"
         assert set(['original_mask', 'diffused_mask', 'time']).issubset(set(y_true.keys())), f"{y_true.keys()=}"
         prob_xtm1_given_xt_x0 = self.theta_post(diffused_mask=y_true['diffused_mask'], original_mask=y_true['original_mask'], time=y_true['time'])
-        prob_xtm1_given_xt_x0pred = self.theta_post_prob(diffused_mask=y_true['diffused_mask'], pred_mask=y_pred, time=y_true['time'])
         mask = y_true['original_mask'] != self.ignore_index
         mask = mask.unsqueeze(-3).expand(-1, self.num_classes, -1, -1)
-        assert mask.shape == prob_xtm1_given_xt_x0.shape == prob_xtm1_given_xt_x0pred.shape
+        assert mask.shape == prob_xtm1_given_xt_x0.shape == y_pred.shape
         prob_xtm1_given_xt_x0 = prob_xtm1_given_xt_x0[mask]
-        prob_xtm1_given_xt_x0pred = prob_xtm1_given_xt_x0pred[mask]
+        y_pred = y_pred[mask]
         loss = torch.nn.functional.kl_div(
-            input=torch.log(torch.clamp(prob_xtm1_given_xt_x0pred, min=1e-12)),
+            input=torch.log(y_pred),
             target=prob_xtm1_given_xt_x0, log_target=False,
             reduction='mean',
         )
-        assert not loss.isnan(), f"{prob_xtm1_given_xt_x0pred.min()=}, {prob_xtm1_given_xt_x0pred.max()=}, {prob_xtm1_given_xt_x0.min()=}, {prob_xtm1_given_xt_x0.max()=}"
+        assert not loss.isnan(), f"{y_pred.min()=}, {y_pred.max()=}, {prob_xtm1_given_xt_x0.min()=}, {prob_xtm1_given_xt_x0.max()=}"
         assert loss.numel() == 1, f"{loss.shape=}"
         self.buffer.append(loss)
         return loss
