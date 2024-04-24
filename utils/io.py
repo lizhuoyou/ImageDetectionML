f0e3cb000063553f85eb06b587206fb9a198d864
Daniel Mao <danielmao2019@gmail.com>
2024-04-24 17:50:18 -0400

    [Utils] Expand load_image utility to gray scale ('L')

--- utils/io.py
+++ utils/io.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Optional
 import os
 import json
 import jsbeautifier
@@ -8,18 +8,25 @@ from PIL import Image
 from .ops import apply_tensor_op
 
 
-def load_image(filepath: str, dtype: torch.dtype) -> torch.Tensor:
+def load_image(filepath: str, dtype: Optional[torch.dtype] = torch.float32) -> torch.Tensor:
     assert type(filepath) == str, f"{type(filepath)=}"
     assert os.path.isfile(filepath), f"{filepath=}"
     assert type(dtype) == torch.dtype, f"{type(dtype)=}"
-    assert dtype in [torch.float32, torch.uint8], f"{dtype=}"
+    assert dtype in [torch.float32, torch.int64, torch.uint8, torch.bool], f"{dtype=}"
+    # read from disk
     image = Image.open(filepath)
-    assert image.mode == "RGB"
-    image = torch.tensor(numpy.array(image)).permute(2, 0, 1)
-    assert len(image.shape) == 3 and image.shape[0] == 3, f"{image.shape=}"
+    # convert to torch.Tensor
+    if image.mode == 'RGB':
+        image = torch.from_numpy(numpy.array(image)).permute(2, 0, 1)
+        assert image.dim() == 3 and image.shape[0] == 3, f"{image.shape=}"
+    elif image.mode == 'L':
+        image = torch.from_numpy(numpy.array(image))
+        assert image.dim() == 2, f"{image.shape=}"
     assert image.dtype == torch.uint8, f"{image.dtype=}, {filepath=}"
+    image = image.type(dtype)
+    # transform
     if dtype == torch.float32:
-        image = image.type(torch.float32) / 255
+        image = image / 255.
     return image
 
 
